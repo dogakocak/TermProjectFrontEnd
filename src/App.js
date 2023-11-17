a7e42a06b465072a70a0112e617716f6a65a97aa
dogakocak <dogakocak>
2023-11-17 13:41:23 +0300

    Login module implemented, cookies used. react router dom used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Login from "./Components/Login/Login";
+import Homepage from "./Components/Homepage/Homepage";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        // Giriş başarılı olduğunda bu fonksiyonu çağırabilirsin.
+        setIsLoggedIn(true);
+    };
+
+    const handleLogout = () => {
+        // Çıkış yapıldığında bu fonksiyonu çağırabilirsin.
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <Routes>
+            <Route path='/login' element={<Login onLogin={handleLogin} />} />
+            {isLoggedIn ? (
+                <Route path='/*' element={<Homepage onLogout={handleLogout} />} />
+            ) : (
+                <Route path='/*' element={<Navigate to='/login' />} />
+            )}
+        </Routes>
+    );
 }
 
 export default App;
