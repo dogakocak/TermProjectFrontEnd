e6a50b3bc6dc0daa8e5b9275809ae41786679811
dogakocak <dogakocak>
2024-01-07 18:59:21 +0300

    Login panel changes

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,23 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './Login.css';
 import user_icon from '../Assets/user_icon.svg';
 import password_icon from '../Assets/password_icon.svg';
 import login_icon from '../Assets/LoginIcon.png';
-
+import { css } from '@emotion/react';
+import { BeatLoader } from 'react-spinners';
 
 import Cookies from 'js-cookie'
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 
-
-
-const Login = ( {onLogin}) => {
+const Login = ({ onLogin }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-
     useEffect(() => {
-
         if (Cookies.get('isLoggedIn') === 'true') {
             onLogin();
             navigate('/');
@@ -33,36 +31,38 @@ const Login = ( {onLogin}) => {
     };
 
     const handleSubmit = async () => {
+        setLoading(true); // İstek başladığında loading durumunu true yap
         const apiUrl = 'https://localhost:7250/login';
 
         const requestBody = {
             email: username,
-            password: password
+            password: password,
         };
 
-        fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestBody)
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.status == 401){
-                    showAlert('Kullanıcı adı veya şifre yanlış', 'error');
-                    return;
-                }
-                Cookies.set('isLoggedIn', true);
-                sessionStorage.setItem('accessToken', data["accessToken"]);
-                navigate('/');
-            })
-            .catch(error => {
-                console.error('Error:', error);
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestBody),
             });
 
+            const data = await response.json();
 
+            if (response.status === 401) {
+                showAlert('Kullanıcı adı veya şifre yanlış', 'error');
+            } else {
+                Cookies.set('isLoggedIn', true);
+                sessionStorage.setItem('accessToken', data['accessToken']);
+                navigate('/');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        } finally {
+            setLoading(false); // İstek tamamlandığında loading durumunu false yap
+        }
     };
 
     const showAlert = (message, type) => {
@@ -70,7 +70,6 @@ const Login = ( {onLogin}) => {
         alertDiv.className = `alert ${type}`;
         alertDiv.innerHTML = message;
 
-        // Assuming you have a container element to hold your alerts
         const alertContainer = document.getElementById('alert-container');
         alertContainer.appendChild(alertDiv);
 
@@ -78,7 +77,6 @@ const Login = ( {onLogin}) => {
             alertContainer.removeChild(alertDiv);
         }, 2000);
     };
-
 
     const handleEnterKeyPress = (event) => {
         if (event.key === 'Enter') {
@@ -91,9 +89,9 @@ const Login = ( {onLogin}) => {
             <div className='header'>
                 <div className='text'>Login to Stock App</div>
                 <div className='underline'></div>
-                <img src={login_icon} alt='Login Icon'/>
+                <img src={login_icon} alt='Login Icon' />
             </div>
-            <div id="alert-container"></div>
+            <div id='alert-container'></div>
             <div className='inputs'>
                 <div className='input'>
                     <input
@@ -104,7 +102,7 @@ const Login = ( {onLogin}) => {
                         onKeyDown={handleEnterKeyPress}
                         autoComplete='off'
                     />
-                    <img src={user_icon} alt='User Icon' className='icon'/>
+                    <img src={user_icon} alt='User Icon' className='icon' />
                 </div>
                 <div className='input'>
                     <input
@@ -116,18 +114,28 @@ const Login = ( {onLogin}) => {
                         name='loginPassword'
                         autoComplete='off'
                     />
-                    <img src={password_icon} alt='Password Icon' className='icon'/>
+                    <img src={password_icon} alt='Password Icon' className='icon' />
                 </div>
             </div>
             <div className='submit-container'>
                 <div className='submit' onClick={handleSubmit}>
-                    Login
+                    {loading ? (
+                        <BeatLoader
+                            color='#fff'
+                            loading={true}
+                            css={css`
+                display: inline-block;
+                margin-top: -4px; // İhtiyaca göre ayarlayabilirsiniz
+              `}
+                        />
+                    ) : (
+                        'Login'
+                    )}
                 </div>
-
             </div>
-            <div className="register-link">
+            <div className='register-link'>
                 <p>
-                    Dont have an account? <Link to="/register">Register</Link>
+                    Dont have an account? <Link to='/register'>Register</Link>
                 </p>
             </div>
             <div className='underline-footer'></div>
